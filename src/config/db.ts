779cefff63b08cc5500f4e85172ad039a6a958e7
javalencis <javalencis@gmail.com>
2025-05-17 23:50:08 -0500

    logs de depuracion

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/miapp';
-
+console.log(MONGO_URI)
 export default async function connectDB() {
   try {
     await mongoose.connect(MONGO_URI);
